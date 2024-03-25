f0a2cfc796a14d2b29da512a49aeb9a1e2596ce3
E2_ASHU-84071 <asonawane00111@gmail.com>
2024-03-25 16:14:58 +0530

    assignment03

--- assignment04/demo05.c
+++ assignment04/demo05.c
@@ -1,30 +1,52 @@
-#include<stdio.h>
-void print_character(char,int)
+/*#include<stdio.h>
 
+void print_character(char ch,int num_times)
+{ 
+for (int i=1 ; i<=num_times ; i++)
+{
+printf("%c",ch);
+}
+printf("\n");
+}
 int main()
 {
+
 int num;
 char ch;
 printf("enter the num:");
 scanf("%d",&num);
 
 printf("enter char:");
-scanf("%c",&ch);
+scanf(" %c", &ch);
 
-return 0;
+print_character(ch,num);
+
+}
+*/
 
 
-void print_character(char ch,int num_times){
-if(num_times<=0){
-return;
+
+#include <stdio.h>
+
+void print_character(char ch, int num_times) {
+  for (int i = 1; i <= num_times; i++) {
+    printf("%c", ch);
+  }
+  printf("\n");
 }
 
-for(i=0;i<num_times;i++){
-printf("%c",ch);
-}
-printf("\n");
-}
-}
+int main() {
+  int num;
+  char ch;
 
+  printf("Enter the number of times to print a character: ");
+  scanf("%d", &num);
 
+  printf("Enter the character to be printed: ");
+  scanf(" %c", &ch); // Add a space before %c to consume leftover newline
+
+  print_character(ch, num);
+
+  return 0;
+}
 
